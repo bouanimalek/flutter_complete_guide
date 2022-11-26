906825d965abf737d701f309c7f1c1f62299200b
malzak <malzovich@gmail.com>
2022-11-26 14:07:47 +0100

    another expression that dart accepts

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       home: Text("Hello!"),
     );
