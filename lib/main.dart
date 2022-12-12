377a8957ffddc4344eff679ae63d53500a9953dc
malek <malzovich@gmail.com>
2022-12-12 19:51:48 +0100

    Mapping Lists to Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,18 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?',
+      {
+        "questionText": 'What\'s your favorite color?',
+        "answers": ["Black", "Red", "Green", "White"]
+      },
+      {
+        "questionText": 'What\'s your favorite animal?',
+        "answers": ["Rabbit", "Snake", "Elephant", "Lion"]
+      },
+      {
+        "questionText": 'Who\'s your favorite instructor?',
+        "answers": ["Malek", "Malex", "Malik", "Max"]
+      },
     ];
     return MaterialApp(
       home: Scaffold(
@@ -39,10 +49,11 @@ class _MyAppState extends State<MyApp> {
           title: Text("My First App"),
         ),
         body: Column(children: [
-          Question(questions[_questionIndex]),
-          Answer(_answerQuestion),
-          Answer(_answerQuestion),
-          Answer(_answerQuestion),
+          Question(questions[_questionIndex]["questionText"]),
+          ...(questions[_questionIndex]["answers"] as List<String>)
+              .map((answer) {
+            return Answer(_answerQuestion, answer);
+          }).toList()
           // ElevatedButton(onPressed: null, child: Text("Answer 1")),
           // ElevatedButton(onPressed: null, child: Text("Answer 2")),
           // ElevatedButton(onPressed: null, child: Text("Answer 3")),
